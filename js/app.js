95a900ef4c80318d1b8303534f5ffde18283d0fb
renn1805 <renan.almeida.arau@gmail.com>
2025-07-03 15:27:31 -0300

    fim projeto

--- js/app.js
+++ js/app.js
@@ -1,49 +1,63 @@
-//? variavel responsevel pelo input
-const inputTask = document.querySelector(".input-task");
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-task')
 
-// ? variavel responsavel pelo botao de adicionar tarefas
-const addTask = document.querySelector(".button-add-task");
+let minhaListaDeItens = []
 
-// ? variavel responsavel pela "ul"
-const completeList = document.querySelector(".list-task");
-//
+function adicionarNovaTarefa() {
+    minhaListaDeItens.push({
+        tarefa: input.value,
+        concluida: false,
+    })
 
-// ? array responsavel por conter todos os nomes das tasks
-let textTask = [];
-// todo: funcition responsavel por ditar os nomes das tasksa
-function adicionarTask() {
-   // responsavel por levar o valor do input até o array
-   textTask.push(inputTask.value);
+    input.value = ''
 
-   //responsavel por chamar a proxima função
-   mostrarTask();
+    mostrarTarefas()
 }
 
-// todo: function responsavel por formar o codigo da lista e coloca-la na tela
-function mostrarTask() {
-   // ? variavel responsavel por conter o codigo html
-   let htmlCode = "";
-   // passa por todos os itens da array de textos e para cada um deles cria seu card
-   Array.from(textTask).forEach((el) => {
-      // atribui a variavel do codigo cada atualização de task
-      htmlCode =
-         htmlCode +
-         //  texto que vai ser colocado no html
-         `
-            <li class="task">
-                <button class="check">
-                    <img src="img/icon-check.png" alt="concuir task" />
-                </button>
-                <h2 class="titulo-task">${el}</h2>
-                <button class="trash">
-                    <img src="img/icon-trash.png" alt="" />
-                </button>
-            </li>
-    `;
-      // comando de escrever o codigo gerado dentro da tag ul
-      completeList.innerHTML = htmlCode;
-   });
+function mostrarTarefas() {
+    let novaLi = ''
+
+    minhaListaDeItens.forEach((el, index) => {
+        novaLi =
+            novaLi +
+            `
+
+        <li class="task ${el.concluida && 'concluida'}">
+            <img src="img/icon-check.png" alt="check-na-tarefa" onclick="concluirTarefa(${index})">
+            <h2>${el.tarefa}</h2>
+            <img src="img/icon-trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${index})">
+        </li>
+        
+        `
+    })
+
+    listaCompleta.innerHTML = novaLi
+
+    localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
 }
 
-// observa os eventos que acontecem com o botao de adicionar tasks, assim todas as ações de click ele faz a função indicada
-addTask.addEventListener("click", adicionarTask);
+function concluirTarefa(posicao) {
+    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+
+    mostrarTarefas()
+}
+
+function deletarItem(posicao) {
+    minhaListaDeItens.splice(posicao, 1)
+
+    mostrarTarefas()
+}
+
+function recarregarTarefas() {
+    const tarefasDoLocalStorage = localStorage.getItem('lista')
+
+    if (tarefasDoLocalStorage) {
+        minhaListaDeItens = JSON.parse(tarefasDoLocalStorage)
+    }
+
+    mostrarTarefas()
+}
+
+recarregarTarefas()
+button.addEventListener('click', adicionarNovaTarefa)
